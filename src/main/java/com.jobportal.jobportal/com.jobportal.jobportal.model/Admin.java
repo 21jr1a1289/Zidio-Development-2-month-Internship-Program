9840acfbfe6f9b5951464cf264b2b914c8fb49a1
Priyanka <melampriyanka1@gmail.com>
2025-07-24 09:36:42 +0530

    Create Admin.java

---
+++ src/main/java/com.jobportal.jobportal/com.jobportal.jobportal.model/Admin.java
@@ -0,0 +1,67 @@
+package com.jobportal.jobportal.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+    private String bio;
+
+    // Getters and Setters
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+}
+
